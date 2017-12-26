029594fdde36bb72606dda202051a3149dec5255
Anonymous Applicant <anonymous@example.com>
2017-12-26 17:11:06 -0500

    hookup 'upcoming' api call to search endpoint

--- src/my_exercise/routes/search.clj
+++ src/my_exercise/routes/search.clj
@@ -1,9 +1,17 @@
 (ns my-exercise.routes.search
   (:require [hiccup.page :refer [html5]]
-            [my-exercise.routes.defaults :refer [header address-form]]))
+            [clj-http.client :as http]
+            [my-exercise.routes.defaults :refer [header address-form]]
+            [my-exercise.ocd-id :as ocd]))
+
+(def api-base "https://api.turbovote.org/elections/upcoming")
 
 (defn page [request]
-  (let [{:keys [street street-2 city state zip]} (:params request)]
+  (let [{:keys [street street-2 city state zip]} (:params request)
+        ocd-ids (ocd/gen-us-ids state city)
+        ocd-id-string (clojure.string/join "," ocd-ids)
+        resp (http/get api-base {:query-params {"district-divisions" ocd-id-string}})
+        results (clojure.edn/read-string (:body resp))]
     (html5
       (header request)
-      (str "vals: " street " " street-2 " " city " " state " " zip))))
+      (str results))))
